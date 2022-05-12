8449bcd714eb27e47d022c34134054ce93444930
Konstantin Yurichev <kyurichev@projectx019.com>
2022-05-12 23:25:06 +0300

    add swift-5.6 package

---
+++ Package@swift-5.5.swift
@@ -0,0 +1,47 @@
+// swift-tools-version: 5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "LocalizeChecker",
+    platforms: [.macOS(.v12)],
+    products: [
+        .executable(name: "check-localize", targets: ["LocalizeCheckerCLI"]),
+        .library(name: "LocalizeChecker", targets: ["LocalizeChecker"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-syntax.git",
+            "0.50600.1"..."0.50600.1"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser",
+            "1.1.2"..."1.1.2"
+        )
+    ],
+    targets: [
+        .executableTarget(
+            name: "LocalizeCheckerCLI",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "LocalizeChecker",
+            ]
+        ),
+        .target(
+            name: "LocalizeChecker",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxParser", package: "swift-syntax")
+            ]
+        ),
+        .testTarget(
+            name: "LocalizeCheckerTests",
+            dependencies: [
+                "LocalizeChecker",
+                .product(name: "SwiftSyntax", package: "swift-syntax")
+            ],
+            path: "Tests",
+            resources: [.copy("Fixtures")]
+        )
+    ]
+)
